1ecd41465dad2bc7ef5d517f2a45e305ab538a35
jucamode <juancarlosmontanod@gmail.com>
2025-02-12 12:06:18 -0700

    Lesson 12

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
