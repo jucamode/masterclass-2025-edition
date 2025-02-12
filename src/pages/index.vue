9cce804adddc721398057e376d4788b89084f8c2
jucamode <juancarlosmontanod@gmail.com>
2025-02-12 12:50:18 -0700

    Lesson 15

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to project 1</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
