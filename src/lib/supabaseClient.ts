d750eb69db446a68361c1b589cafac87f3b37f2c
jucamode <juancarlosmontanod@gmail.com>
2025-02-17 15:06:05 -0700

    Lesson 24

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://juzawcmxtyajkfuvsjia.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1emF3Y214dHlhamtmdXZzamlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzOTA1NDcsImV4cCI6MjA1NDk2NjU0N30.JlqScCeyxZ-9mfdCWWvAbob_oV47e_cFUYAfLDVm4nk',
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
 )
