5d644060a65154b1d6a07d500e3358df82d4bdf1
Juan Carlos <juancarlosmontanod@gmail.com>
2025-02-18 00:10:01 -0700

    Lesson 24 pt 2

--- database/seed.js
+++ database/seed.js
@@ -6,4 +6,15 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+  const current_status = await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLowerCase().replace(/ /g, '-'),
+    //status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    status: faker.helpers.enumValue(current_status),
+    collaborators: faker.helpers.arrayElements([1, 2, 3]),
+  })
+}
+
+await seedProjects()
