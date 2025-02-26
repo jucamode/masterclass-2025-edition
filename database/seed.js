3bb6133d1296a2d488d8b33c48d00a0f1eae18cb
jucamode <juancarlosmontanod@gmail.com>
2025-02-26 14:10:41 -0700

    Lesson 25 FINISHED
    Error was a comma after the variable in the .env file

--- database/seed.js
+++ database/seed.js
@@ -3,16 +3,14 @@
 import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
 const seedProjects = async () => {
   const name = faker.lorem.words(3)
-  const current_status = await supabase.from('projects').insert({
+  await supabase.from('projects').insert({
     name: name,
     slug: name.toLowerCase().replace(/ /g, '-'),
-    //status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    status: faker.helpers.enumValue(current_status),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
     collaborators: faker.helpers.arrayElements([1, 2, 3]),
   })
 }
