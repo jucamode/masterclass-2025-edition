a69a75edc82625c94feab25333d20fa3eea6b134
jucamode <juancarlosmontanod@gmail.com>
2025-02-26 15:04:35 -0700

    Lesson 30

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,10 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref<any[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) console.log(error)
+  projects.value = data
+  console.log('projects: ', projects.value)
+})()
+</script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project. }}
+      </li>
+    </ul>
   </div>
 </template>
 
