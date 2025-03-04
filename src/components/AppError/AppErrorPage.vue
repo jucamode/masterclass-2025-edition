d2e8b4e32715eff1be9c5bbb9a6f57fabee59dd4
jucamode <juancarlosmontanod@gmail.com>
2025-03-04 14:38:06 -0700

    Lesson 69

--- src/components/AppError/AppErrorPage.vue
+++ src/components/AppError/AppErrorPage.vue
@@ -27,15 +27,18 @@ if (error.value && 'code' in error.value) {
   statusCode.value = error.value.statusCode ?? 0
 }
 
+const ErrorTemplate = import.meta.env.DEV
+  ? defineAsyncComponent(() => import('./AppErrorDevSection.vue'))
+  : defineAsyncComponent(() => import('./AppErrorProdSection.vue'))
+
 router.afterEach(() => {
-  errorStore.activeError = null
+  errorStore.clearError()
 })
 </script>
 
 <template>
   <section class="error">
-    <AppErrorDevSection :message :customCode :code :statusCode :hint :details />
-    <!-- <AppErrorProdSection
+    <ErrorTemplate
       :message
       :customCode
       :code
@@ -43,7 +46,7 @@ router.afterEach(() => {
       :hint
       :details
       :isCustomError="errorStore.isCustomError"
-    /> -->
+    />
   </section>
 </template>
 
