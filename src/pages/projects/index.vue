0a32fac75fb81ad0afc43e2fbcf507b58558d0fc
Juan Carlos <juancarlosmontanod@gmail.com>
2025-03-03 19:31:18 -0700

    Lesson 51

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -6,12 +6,13 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
 const projects = ref<Tables<'projects'>[] | null>(null)
-;(async () => {
+const getProjects = async () => {
   const { data, error } = await supabase.from('projects').select()
   if (error) console.log(error)
   projects.value = data
-  console.log('projects: ', projects.value)
-})()
+}
+
+await getProjects()
 
 const columns: ColumnDef<Tables<'projects'>>[] = [
   {
